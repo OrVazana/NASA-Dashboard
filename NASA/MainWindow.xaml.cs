7ccca8405bf86369c3ce574364b57ad005683b50
Or Vazana <73831613+OrVazana@users.noreply.github.com>
2021-10-01 05:33:04 +0300

    second commit

---
+++ NASA/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NASA
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        #region top Bar buttons 
+        private void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private void ButtonMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+        private void ButtonMaximaize_Click(object sender, RoutedEventArgs e)
+        {
+            Window window = Application.Current.MainWindow;
+            if (window.WindowState == System.Windows.WindowState.Maximized)
+                window.WindowState = System.Windows.WindowState.Normal;
+            else
+                window.WindowState = System.Windows.WindowState.Maximized;
+        }
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+        #endregion
+
+    }
+}
