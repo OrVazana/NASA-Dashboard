9a03b9f9b16b8bbddf908b25e49b113df55dff97
Or Vazana <73831613+OrVazana@users.noreply.github.com>
2021-10-04 14:12:55 +0300

    few files

---
+++ NASA/Views/SolarSystemView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace NASA.Views
+{
+    /// <summary>
+    /// Interaction logic for SolarSystemView.xaml
+    /// </summary>
+    public partial class SolarSystemView : UserControl
+    {
+        public SolarSystemView()
+        {
+            InitializeComponent();
+        }
+    }
+}
