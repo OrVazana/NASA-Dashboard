09ee9197e08d0222ee4389d73c325b2ec8e2a336
Refael Bardugo <rebardug@gmail.com>
2021-10-10 19:56:51 +0300

    nothing

---
+++ NASA/Commands/PlanetDescription.cs
@@ -0,0 +1,38 @@
+﻿using NASA.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace NASA.Commands
+{
+    public class PlanetDescriptionCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public SolarSystemVM CurrentVM { get; set; }
+
+        public PlanetDescriptionCommand(SolarSystemVM VM)
+        {
+            CurrentVM = VM;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            int.TryParse(parameter.ToString(), out int Days);
+            //CurrentVM.getSelectedPlanet();
+        }
+    }
+}
+
